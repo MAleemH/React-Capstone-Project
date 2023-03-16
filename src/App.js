af6ead08a262a53e49a5a2694989c086a70baf0c
MAleemH <aleemhashmi4321@gmail.com>
2023-03-16 13:56:14 +0500

    Added react components with functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/layout';
+import City from './components/city';
+import Detail from './components/detail';
+import NotMatch from './components/notMatch';
 
 function App() {
   return (
     <div>
-      Hello World!
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<City />} />
+          <Route path="/detail/:name/:lat/:lon" element={<Detail />} />
+          <Route path="*" element={<NotMatch />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
